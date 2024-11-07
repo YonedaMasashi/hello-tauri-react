cb258072ef25f45ca4bdfe8f7e7f4dd1410de461
Masashi Yoneda <pillows916@gmail.com>
2024-11-08 01:25:01 +0900

    MUI を使ってタブビューを更新

--- hello-tauri-react/src/MainView.tsx
+++ hello-tauri-react/src/MainView.tsx
@@ -1,51 +1,36 @@
 import React, {useState} from 'react'
 import StudentInfoInputTab from './compenents/StudentInfoInputTab';
 import StudentListTab from './compenents/StudentListTab';
-
-
-type Tab = {
-    label: string;
-    content: React.ReactNode;
-}
+import {
+    Box,
+    Tab,
+    Tabs,
+  } from '@mui/material';
 
 const tabViewList: Tab[] = [
-    { label: 'Input', content: <StudentInfoInputTab />},
-    { label: 'Student List', content: <StudentListTab />}
+    { label: '入力', content: <StudentInfoInputTab />},
+    { label: '一覧', content: <StudentListTab />}
 ];
 
 const MainView: React.FC = () => {
-    const [activeTabIndex, setActiveTabIndex] = useState<number>(0);
-    
-    const handleTabClick = (index: number) => {
-        setActiveTabIndex(index);
+    const [tabValue, setTabValue] = useState(0);
+
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        setTabValue(newValue);
     };
 
     return (
-        <div>
-            <div style={{ display: 'flex', borderBottom: '1px solid #ccc' }}>
+        <Box padding={2} width="400px"> 
+            <Tabs value={tabValue} onChange={handleTabChange}>
                 {
                     tabViewList.map((tabView, index) => (
-                        <button
-                            key={index}
-                            onClick={() => handleTabClick(index)}
-                            style={{
-                                padding: '10px 20px',
-                                cursor: 'pointer',
-                                borderBottom: activeTabIndex === index ? '2 solid blue' : 'none',
-                                backgroundColor: activeTabIndex === index ? '#CCB3DD' : 'transparent',
-                                border: 'none',
-                                outline: 'none'
-                            }}
-                        >
-                            { tabView.label }
-                        </button>
+                        <Tab label={ tabView.label } />
                     ))
                 }
-            </div>
-            <div style={{ padding: '10px' }}>
-                { tabViewList[activeTabIndex].content }
-            </div>
-        </div>
+            </Tabs>
+
+            { tabViewList[tabValue].content }
+        </Box>
     );
 };
 
